929334d9765431c4ba4c85b581b445ed51b51744
jancimertel <johnny.mert@gmail.com>
2019-07-04 02:29:02 +0200

    *update* refactored

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Bridge will first use the Exporter module to connect to zookeeper instance.
 Then it will connect to dedicated cryptomood api server.
 
 Check the `docker-compose.yml` file for env variables. 
+
+| name | example value | desctiption | 
+|---|---|---|
+| CRYPTOMOOD_URL | aaa.bbb.ccc:1234 | url that has been provided for you |
+| CERT_FILE_PATH | ./cert.pem | path to .pem file |
+| PROTO_FILE_PATH | ./types.proto | path to definitions |
+| ONLY_HISTORIC | 0 | 1 = will only process historic data, 0 = same as 1, but will also subscribe for new data |
+| CANDLE_TYPE | social | social or news |
--- index.js
+++ index.js
@@ -8,43 +8,87 @@ const exporter = new Exporter(sanExporterPkg.name);
 const SERVER = process.env.CRYPTOMOOD_URL;
 const CERT_FILE_PATH = process.env.CERT_FILE_PATH;
 const PROTO_FILE_PATH = process.env.PROTO_FILE_PATH;
+const ONLY_HISTORIC = process.env.ONLY_HISTORIC === "1";
+const CANDLE_TYPE = process.env.CANDLE_TYPE;
 
-const SOCIAL_CANDLE_TYPE = {
-  NEWS: 'news',
-  SOCIAL: 'social',
+const CANDLE_TYPES = {
+  news: 'news',
+  social: 'social',
+};
+
+const STATES = {
+  INIT: -1,
+  PROCESSING_HISTORIC_DATA: 0,
+  PROCESSING_BUFFERED_DATA: 1,
+  NORMAL: 2,
 };
 
 class CryptomoodSanExporter {
-  constructor() {
+  constructor(type) {
+    this.test = [];
+    this.state = STATES.INIT;
     this.proto = null;
-    this.client = null;
-    this.socialSentimentChannel = null;
-    this.newsSentimentChannel = null;
+    this.subClient = null;
+    this.histClient = null;
 
-    this.onSocialData = this.onData.bind(this, SOCIAL_CANDLE_TYPE.SOCIAL);
-    this.onNewsData = this.onData.bind(this, SOCIAL_CANDLE_TYPE.NEWS);
+    this.buffer = {};
+
+    if (!CANDLE_TYPES[type]) {
+      throw new Error("unknown candle type");
+    }
+
+    this.type = type;
+
+    this.sentimentChannel = null;
   }
 
-  async onData(type, sentimentData) {
-    sentimentData.type = type;
-    sentimentData.id = type + "_" + sentimentData.id;
+  normalizeCandle(sentimentData) {
+    sentimentData.type = this.type;
+    sentimentData.id = this.type + "_" + sentimentData.id;
     sentimentData.start_time = sentimentData.start_time.seconds;
+  }
 
-    await exporter.sendDataWithKey(sentimentData, "id");
-    console.log('ok', sentimentData.id)
+  async onData(sentimentData, force) {
+    this.normalizeCandle(sentimentData);
+
+    if (force || sentimentData.updated) {
+     // await exporter.sendDataWithKey(sentimentData, "id");
+    }
   }
 
   async run() {
     await exporter.connect();
+    //await exporter.savePosition(null) // reset
 
     try {
-      await this.subscribe(this.onSocialData, this.onNewsData);
+      await this.connectCryptomood();
     } catch (e) {
       console.log(e);
+      return false;
+    }
+
+    if (!ONLY_HISTORIC) {
+      if (this.type === CANDLE_TYPES.social) {
+        this.subscribeToSocialSentiment();
+      } else {
+        this.subscribeToNewsSentiment();
+      }
+    }
+
+    try {
+      await this.processOlderData();
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+
+    this.state = STATES.NORMAL;
+    if (ONLY_HISTORIC) {
+      process.exit();
     }
   }
 
-  subscribe(socialCb, newsCb) {
+  connectCryptomood() {
     this.proto = grpc.loadPackageDefinition(
       protoLoader.loadSync(PROTO_FILE_PATH, {
         keepCase: true,
@@ -55,26 +99,222 @@ class CryptomoodSanExporter {
       })
     );
 
-    this.client = new this.proto.MessagesProxy(
+    this.subClient = new this.proto.MessagesProxy(
+      SERVER,
+      grpc.credentials.createSsl(fs.readFileSync(CERT_FILE_PATH)),
+    );
+
+    this.histClient = new this.proto.HistoricData(
       SERVER,
       grpc.credentials.createSsl(fs.readFileSync(CERT_FILE_PATH)),
     );
 
     return new Promise((resolve, reject) => {
-      grpc.waitForClientReady(this.client, new Date().getTime() + 60000, (err) => {
+      grpc.waitForClientReady(this.subClient, new Date().getTime() + 60000, (err) => {
         if (err) {
           return reject(err);
+        } else {
+          return resolve();
         }
-        console.log("Cryptomood connected")
-        this.socialSentimentChannel = this.client.SubscribeSocialSentiment();
-        this.socialSentimentChannel.on("data", socialCb);
-
-        this.newsSentimentChannel = this.client.SubscribeNewsSentiment();
-        this.newsSentimentChannel.on("data", newsCb);
       });
+    }).then(() => new Promise((resolve, reject) => {
+      grpc.waitForClientReady(this.histClient, new Date().getTime() + 60000, (err) => {
+        if (err) {
+          return reject(err);
+        } else {
+          return resolve();
+        }
+      });
+    }))
+  }
+
+  async getHistoricRange() {
+    const method = this.type === CANDLE_TYPES.SOCIAL ? this.histClient.HistoricSocialSentimentRange : this.histClient.HistoricNewsSentimentRange;
+
+    return new Promise((resolve, reject) => {
+      method.apply(this.histClient, [null, function (err, req) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(req);
+        }
+      }]);
+    })
+  }
+
+  async getHistoricData(config) {
+    const method = this.type === CANDLE_TYPES.SOCIAL ? this.histClient.HistoricSocialSentiment : this.histClient.HistoricNewsSentiment;
+
+    return new Promise((resolve, reject) => {
+      method.apply(this.histClient, [config, function (err, req) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(req.items);
+        }
+      }]);
+    })
+  }
+
+  /**
+   * Awaitable timeout
+   * @param duration
+   * @returns {Promise<any>}
+   */
+  sleep(duration) {
+    console.log(`Sleeping - waiting for ${duration}ms (for the second half of current minute)`);
+    return new Promise((resolve) => {
+      setTimeout(resolve, duration);
     });
+  }
+
+  /**
+   * Until streaming is done on the api side, we need to paginate
+   * @returns {Promise<void>}
+   */
+  async processOlderData() {
+    this.state = STATES.PROCESSING_HISTORIC_DATA;
+
+    let firstTimestamp = await exporter.getLastPosition();
+    let upToTimestamp;
+
+    if (!firstTimestamp) {
+      const possibleDataRange = await this.getHistoricRange();
+      firstTimestamp = parseInt(possibleDataRange.first.seconds);
+    }
+
+    let currentTimestamp = new Date();
+
+    // waiting for the second halfminute if required - to be sure we wont be missing a candle
+    if (currentTimestamp.getSeconds() < 30) {
+      await this.sleep((30 - currentTimestamp.getSeconds()) * 1000);
+    }
+
+    // rounded to minute - if currentTimestamp is 17:45:01, then upToTimestamp will be 17:46:00
+    // - 45th minute should be (after sleep call) in the storage
+    // the range will be <firstTimestamp; upToTimestamp)
+    currentTimestamp.setSeconds(0);
+    currentTimestamp.setMilliseconds(0);
+    currentTimestamp = currentTimestamp.getTime() / 1000;
+    upToTimestamp = currentTimestamp + 60;
+
+    console.log("Possible data range", new Date(firstTimestamp * 1000), new Date(upToTimestamp * 1000));
+
+    const windowSize = 300; // by 5 minute steps
+
+    let windowStart = firstTimestamp;
+    let windowEnd = windowStart;
+
+    while (windowStart < upToTimestamp) {
+      windowEnd += windowSize;
+
+      const data = await this.getHistoricData({
+        from: {seconds: windowStart}, // greater or equal condition
+        to: {seconds: Math.min(windowEnd, upToTimestamp)}, // less than condition
+        resolution: "M1",
+        allAssets: true
+      });
+
+      console.log('added', new Date(windowStart * 1000), new Date(Math.min(windowEnd, upToTimestamp) * 1000));
+      windowStart = windowEnd;
+
+      let lastPosition = windowEnd;
+      for (const candle of data) {
+        await this.onData(candle, true);
+        lastPosition = parseInt(candle.start_time) + 60;
+      }
+      await exporter.savePosition(lastPosition);
+    }
+
+    this.processBuffered();
+    console.log('Historical data processed');
+  }
+
+  async processBuffered() {
+    this.state = STATES.PROCESSING_BUFFERED_DATA;
+
+    const bufferedTimes = Object.keys(this.buffer).sort();
+    console.log("Buffered", bufferedTimes.length);
+    for (const time of bufferedTimes) {
+      console.log("BUFFERED TIME", time);
+      for (const candle of this.buffer[time]) {
+        await this.onData(candle);
+      }
+    }
+
+    this.state = STATES.NORMAL;
+  }
+
+  /**
+   * Will stop any incoming candle while processing buffered items
+   * @returns {Promise<*>}
+   */
+  async waitUntilBufferProcessed() {
+    return new Promise((resolve) => {
+      var int = setInterval(() => {
+        if (this.state === STATES.NORMAL) {
+          clearInterval(int);
+          resolve();
+        }
+      }, 1000);
+    });
+  }
+
+  subscribeToSocialSentiment() {
+    this.sentimentChannel = this.subClient.SubscribeSocialSentiment();
+    this.sentimentChannel.on("data", this.channelCb.bind(this));
+  }
+
+  subscribeToNewsSentiment() {
+    this.sentimentChannel = this.subClient.SubscribeSocialSentiment();
+    this.sentimentChannel.on("data", this.channelCb.bind(this));
+  }
+
+  async channelCb(candle) {
+    const currentTime = parseInt(candle.start_time.seconds);
+    if (candle.resolution !== "M1") {
+      return;
+    }
+    switch (this.state) {
+      case STATES.PROCESSING_HISTORIC_DATA:
+        if (!this.buffer[currentTime]) {
+          this.buffer[currentTime] = [];
+        }
+        this.buffer[currentTime].push(candle);
+        break;
+
+      case STATES.PROCESSING_BUFFERED_DATA:
+        await this.waitUntilBufferProcessed();
+        this.onData(candle);
+        break;
+
+      case STATES.NORMAL:
+        this.onData(candle);
+        try {
+          await exporter.savePosition(currentTime);
+        } catch (e) {
+          console.warn("Error while saving position", currentTime)
+        }
+        break;
+    }
+  }
+
+  testdat() {
+    const interval = 60
+    let lastNumero;
+    let fail = false;
+    for (const time of this.test) {
+      const number = parseInt(time);
+      if (lastNumero) {
+        if (lastNumero + interval !== number) {
+          console.log("error", lastNumero, number);
+          fail = true;
+        }
+      }
+      lastNumero = number;
+    }
   }
 }
 
-const cr = new CryptomoodSanExporter()
+const cr = new CryptomoodSanExporter(CANDLE_TYPE);
 cr.run();
